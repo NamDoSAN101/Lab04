255b4300b68933a2bc21a1035b7d3495961f1200
NamDoSAN101 <bavornthat.1208@mail.kmutt.ac.th>
2022-02-22 13:33:08 +0700

    format

--- jsTicTacToe.js
+++ jsTicTacToe.js
@@ -6,28 +6,29 @@ const random = () =>  comRan = Math.floor(Math.random() * 9) + 1 ;
 
 
 
-for(let i = 1 ;i<10;i++){
-const boxPlayer = document.getElementById(i);
+for (let i = 1 ; i<10; i++){
+    const boxPlayer = document.getElementById(i);
 
-boxPlayer.addEventListener('click', () => {
-    alert(i)
-    boxPlayer.innerText = player
-data[i-1] = player;
+    boxPlayer.addEventListener('click', () => {
+        alert(i)
+        boxPlayer.innerText = player
+    data[i-1] = player;
 
-if(data.indexOf("") > -1){
-do{
-    random();
-}while(
- data[comRan-1] !== ""
-)
-const boxCom = document.getElementById(comRan);
-boxCom.innerText = com ;
-data[comRan-1] = com 
-}
-alert(data);
-}
+    if (data.indexOf("") > -1){
+        do {
+            random();
+        } while(data[comRan-1] !== "")
 
-);
+    const boxCom = document.getElementById(comRan);
+    boxCom.innerText = com ;
+    data[comRan-1] = com 
+    }
+
+    alert(data);
+    
+    }
+
+    );
 }
 
 
